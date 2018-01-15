55d1ee20842878293b0d4b03e1ea80c9c0fd2882
RiceGnocchi <30733238+RiceGnocchi@users.noreply.github.com>
2018-01-15 21:04:06 +0800

    Initial Commit

---
+++ List/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  List
+//
+//  Created by 张旭 on 2018/1/15.
+//  Copyright © 2018年 brx. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
